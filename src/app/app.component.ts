2349024848fec73152ffd3a2f37b7478b1fe4a6e
Vaibhav <vaibhav@microfocus.com>
2019-09-21 13:19:45 +0530

    Angular:NgIf

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'First Application';
+  userName = 'VJ Tech Diary';
+  titleFlag = true;
+  userFlag = false;
+}
